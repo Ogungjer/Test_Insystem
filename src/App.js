c2052398cb78fb6076fb5a454b118c69ce198074
Jeremaye <jeremaye@jeremaye-HP-EliteBook-840-G2>
2022-07-19 17:07:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import Header from './Component/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://hubeau.eaufrance.fr/api/v1/temperature/chronique?code_departement=33&size=5&sort=desc&pretty")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result['data']);
+          this.setState({
+            isLoaded: true,
+            items: result['data']
+          });
+        },
+        
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Chargement...</div>;
+    } else {
+      
+      return (
+        <div>
+          <Header />
+          <ul>
+            {items.map(item => (
+              <li key={item.name}>
+                {item.date_mesure_temp} à {item.heure_mesure_temp} =  {item.resultat} {item.symbole_unite}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+      
+    }
+  }
+
 }
 
 export default App;
---
+++ src/Component/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.css"
+
+function Header() {
+    return(
+        <div>
+            <h2 className="office">Office du tourisme Gironde</h2>
+            <h3 className="title"> les 5 derniers relevés de température des cours d'eau du département de la Gironde</h3>
+        </div>
+
+    )
+}
+
+export default Header
